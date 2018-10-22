ee1867d35aebbc24365b04b101a3fcaae09cf498
Emerson Siega <emerson.siega@ciss.com.br>
2018-10-22 15:11:25 -0300

    feat: Pass prop down to components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const paddingLeft = size => ({
+    paddingLeft: `${size}px`
+})
+
+const Parent = ({name}) => {
+    return (
+        <div style={paddingLeft(10)}>
+            <h1>Parent</h1>
+            <Child name={name}/>
+        </div>
+    )
+}
+
+const Child = ({name}) => {
+    return (
+        <div style={paddingLeft(15)}>
+            <h1>Child</h1>
+            <Grandchild name={name}/>
+        </div>
+    )
+}
+
+const Grandchild = ({name}) => {
+    return (
+        <div style={paddingLeft(20)}>
+            <h1>Grandchild</h1>
+            <h3>Name: {name}</h3>
+        </div>
+    )
+}
+
+class App extends Component {
+    render() {
+        const name = 'Emerson'
+
+        return (
+            <Parent name={name}/>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
